f2f90289ee8b149c6cebd4dfb7a101ee637be075
dro-1 <lorddro1532@gmail.com>
2021-02-01 17:21:44 +0100

    Changed API link 2

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,7 +5,7 @@ const axiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  baseURL: "https://dro-notes-api.herokuapp.com//graphql",
+  baseURL: "https://dro-notes-api.herokuapp.com/graphql",
 });
 axiosInstance.interceptors.request.use(
   (config) => {
@@ -13,24 +13,24 @@ axiosInstance.interceptors.request.use(
     let csrfToken = localStorage.getItem("csrfToken") || "";
     const decoded: any = decode(csrfToken);
     console.log(decoded.exp);
-    //if (new Date(Number(decoded.exp + "000")) < new Date()) {
-    axios
-      .get("https://dro-notes-api.herokuapp.com/refresh-token", {
-        headers: {
-          "Content-Type": "application/json",
-          csrf_refresh_token: csrfRefreshToken,
-        },
-        withCredentials: true,
-      })
-      .then((response) => {
-        csrfToken = response.data.csrfToken;
-        localStorage.setItem("csrfToken", response.data.csrfToken);
-        localStorage.setItem(
-          "csrfRefreshToken",
-          response.data.csrfRefreshToken
-        );
-      });
-    //}
+    if (new Date(Number(decoded.exp + "000")) < new Date()) {
+      axios
+        .get("https://dro-notes-api.herokuapp.com/refresh-token", {
+          headers: {
+            "Content-Type": "application/json",
+            csrf_refresh_token: csrfRefreshToken,
+          },
+          withCredentials: true,
+        })
+        .then((response) => {
+          csrfToken = response.data.csrfToken;
+          localStorage.setItem("csrfToken", response.data.csrfToken);
+          localStorage.setItem(
+            "csrfRefreshToken",
+            response.data.csrfRefreshToken
+          );
+        });
+    }
     config.headers = {
       ...config.headers,
       csrf_token: csrfToken,
