3881463db2f90ab77cb6e93fa1a0a70b0b6a12f8
dro-1 <lorddro1532@gmail.com>
2021-02-01 11:17:18 +0100

    Added search and logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
-import { Switch, Route } from 'react-router-dom'
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom'
 import EditNote from './components/edit-note/edit-note.component';
 
 import HomePage from "./components/home/home.component";
@@ -7,13 +8,32 @@ import LoginPage from './components/login/login.component';
 import ViewNote from './components/notes/view-note.component';
 import SignUpPage from "./components/signup/signup.component";
 
+
+
+const PrivateRoute = ({ component, render, path }: { component?: any, render?: any, path: string }) => {
+  let Component: any;
+  if (component) {
+    Component = component
+  }
+  const isSignedIn = !!localStorage.getItem('csrfToken')
+  return isSignedIn
+    ? render
+      ? <Route path={path} render={render} />
+      : <Route render={props => <Component {...props} />} />
+    : <Redirect
+      to='/login' />
+}
+
+
+
 function App() {
   return <Switch>
     <Route path='/signup' component={SignUpPage} />
     <Route path='/login' component={LoginPage} />
-    <Route path='/home' component={HomePage} />
-    <Route path='/view' render={(props: any) => <ViewNote {...props.location.state.note} />} />
-    <Route path='/edit' render={(props: any) => props.location.state && props.location.state.note ? <EditNote componentType='edit' {...props.location.state.note} /> : <EditNote componentType='add' />} />
+
+    <PrivateRoute path='/home' component={HomePage} />
+    <PrivateRoute path='/view' render={(props: any) => <ViewNote {...props.location.state.note} />} />
+    <PrivateRoute path='/edit' render={(props: any) => props.location.state && props.location.state.note ? <EditNote componentType='edit' {...props.location.state.note} /> : <EditNote componentType='add' />} />
   </Switch>
 }
 
--- src/context/user.provider.tsx
+++ src/context/user.provider.tsx
@@ -5,9 +5,8 @@ export const UserContext = createContext({
   username: "",
   notes: [] as Array<INote>,
   setNotes: (notes: React.SetStateAction<INote[]>) => { },
-  setToken: (token: React.SetStateAction<string>) => { },
   setUsername: (username: React.SetStateAction<string>) => { },
-  token: ''
+  setSearchText: (text: React.SetStateAction<string>) => { },
 });
 
 interface Props {
@@ -17,21 +16,16 @@ interface Props {
 const UserProvider: React.FC<Props> = ({ children }) => {
 
   const [username, setUsername] = useState('')
-  const [token, setToken] = useState('')
   const [notes, setNotes] = useState<Array<INote>>([])
-  const filterNotes = ({ text }: { text: string }) => {
-    const filteredNotes = notes.filter(post => post.title.includes(text))
-    setNotes(filteredNotes)
-  }
+  const [searchText, setSearchText] = useState('')
 
   return (
     <UserContext.Provider value={{
       username,
-      notes,
-      token,
+      notes: notes.filter(post => post.title.toLowerCase().includes(searchText.toLowerCase())),
       setNotes: (notes: React.SetStateAction<INote[]>) => setNotes(notes),
-      setToken: (token: React.SetStateAction<string>) => setToken(token),
-      setUsername: (username: React.SetStateAction<string>) => setUsername(username)
+      setUsername: (username: React.SetStateAction<string>) => setUsername(username),
+      setSearchText: (text: React.SetStateAction<string>) => setSearchText(text),
     }}>
       {children}
     </UserContext.Provider>
